d8d73ccc264b6a759f955fe5859a60a203fca467
lw9916 <lw@blask.cn>
2021-03-01 17:04:57 +0800

    增加用户查询功能

--- src/main/java/com/demo/login/controller/QueryController.java
+++ src/main/java/com/demo/login/controller/QueryController.java
@@ -1,4 +1,38 @@
 package com.demo.login.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.demo.login.bean.QueryUser;
+import com.demo.login.bean.User;
+import com.demo.login.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
 public class QueryController {
+
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping("/queryUser")
+    public String queryAllUserInfo(User user, QueryUser queryUser){
+
+        HashMap<String,Object> result = new HashMap<>();
+        if(userDao.getUserRole(user.getUsername()).equals("admin")){
+            List<User> queryList = userDao.queryAllUser(queryUser.getQueryName());
+            result.put("status","200");
+            result.put("message","Success");
+            result.put("value",queryList);
+            System.out.println(queryList);
+        }else{
+            result.put("status","400");
+            result.put("message","No permission");
+            result.put("value",null);
+        }
+        return JSON.toJSONString(result);
+    }
+
 }
--- src/main/java/com/demo/login/dao/UserDao.java
+++ src/main/java/com/demo/login/dao/UserDao.java
@@ -3,6 +3,10 @@ package com.demo.login.dao;
 import com.demo.login.bean.User;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserDao {
-    public User getUserInfo(@Param("username") String username,@Param("password") String password);
+    User getUserInfo(@Param("username") String username,@Param("password") String password);
+    List<User> queryAllUser(@Param("queryName") String queryName);
+    String getUserRole(@Param("username") String username);
 }
